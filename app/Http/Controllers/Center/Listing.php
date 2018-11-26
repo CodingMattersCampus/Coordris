1b09eb4d1fdbef9d1cc9fdb13bf6288a9668f5a8
gabbydgab <gamba@gabbydgab.com>
2018-11-26 10:05:12 +0800

    fixed center registration form listing

--- app/Http/Controllers/Center/Listing.php
+++ app/Http/Controllers/Center/Listing.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Center;
 
 use App\Http\Controllers\Controller;
+use App\Models\Center\Infrastructure;
+use App\Models\Location\Barangay;
+use App\Models\Location\City;
 use Illuminate\View\View;
 
 class Listing extends Controller
 {
     public function __invoke() : View
     {
-        return \view('center.listing');
+        $cities = City::all();
+        $barangays = Barangay::all();
+        $infrastructures = Infrastructure::all();
+
+        return \view('center.listing', \compact('cities', 'barangays', 'infrastructures'));
     }
 }
--- resources/views/center/listing.blade.php
+++ resources/views/center/listing.blade.php
@@ -1,29 +1,55 @@
 @extends('adminlte::page')
-
 @section('title', 'COORDRIS - Center Detail')
-
 @section('content_header')
     <h1>Center Registration Page</h1>
 @stop
-
 @section('content')
-<div class="row">
-    <form action="{{\route('centers.registration')}}" method="POST">
-        @csrf
-        @if ($errors->any())
-            <div class="alert alert-danger text-xs">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li class="text-sm">{{ $error }}</li>
-                    @endforeach
-                </ul>
+<div id="app" class="row">
+    <div class="col-md-3">
+        <form action="{{\route('centers.registration')}}" method="POST">
+            @csrf
+            @if ($errors->any())
+                <div class="alert alert-danger text-xs">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class="text-sm">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <div class="form-group">
+                <label for="#centerName">Center Name:</label>
+                <input class="form-control" type="text" name="name" id="centerName" placeholder="Example: Sendong Survivors Center">
             </div>
-        @endif
-        <input type="text" name="name">
-        <input type="text" value="1" name="barangay_id">
-        <input type="text" value="1" name="city_id">
-        <input type="text" value="1" name="infrastructure_id">
-        <button> Register</button>
-    </form>
+            <div class="form-group">
+                <label for="#city">Select City:</label>
+                <select id="city" name="city_id" class="form-control">
+                    @foreach($cities as $city)
+                        <option class="form-control" value="{{$city->id}}"> {{$city->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="#barangay">Select Baranagay:</label>
+                <select id="barangay" name="barangay_id" class="form-control">
+                    @foreach($barangays as $barangay)
+                        <option class="form-control" value="{{$barangay->id}}"> {{$barangay->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="#infrastructure">Select Infrastructure:</label>
+                <select id="infrastructure" name="infrastructure_id" class="form-control">
+                    @foreach($infrastructures as $infrastructure)
+                        <option class="form-control" value="{{$infrastructure->id}}"> {{$infrastructure->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-goup">
+                <button class="btn btn-block btn-primary">Register</button>
+            </div>
+        </form>
+    </div>
+    <div class="col-md-8"></div>
 </div>
 @stop
