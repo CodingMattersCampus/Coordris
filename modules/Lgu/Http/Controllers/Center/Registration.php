fbc1cb7f3d447f51670cf57b3b10b6d5e9a56121
gabbydgab <gamba@gabbydgab.com>
2018-11-28 08:27:19 +0800

    fixed automatic city_id getter based on the current logged in user

--- modules/Lgu/Http/Controllers/Center/Registration.php
+++ modules/Lgu/Http/Controllers/Center/Registration.php
@@ -7,14 +7,17 @@ use App\Models\Center;
 use App\Models\User\User;
 use Illuminate\Http\RedirectResponse;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class Registration extends Controller
 {
     public function __invoke(CenterRegistrationRequest $request) : RedirectResponse
     {
+        $user = Auth::guard('lgu')->user();
+
         $data = $request->post();
         $data['slug'] = str_slug($data['name']);
-        $data['city_id'] = User::getCityId();
+        $data['city_id'] = $user->getCityId();
         unset ($data['_token']);
 
         $center = Center::firstOrCreate($data);
