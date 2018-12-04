7a231054e36045e389349ddb7d37117ccebab392
gabbydgab <gamba@gabbydgab.com>
2018-12-04 13:49:45 +0800

    fixed category to main category

--- modules/Lgu/Http/Controllers/Product/Category/Listing.php
+++ modules/Lgu/Http/Controllers/Product/Category/Listing.php
@@ -5,7 +5,7 @@ namespace Modules\Lgu\Http\Controllers\Product\Category;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Product\Category\Category as CategoryResource;
 use App\Http\Resources\Product\Category\CategoryCollection;
-use App\Models\Product\Category;
+use App\Models\Product\Category\MainCategory;
 use Illuminate\Database\Eloquent\Collection;
 use Yajra\DataTables\DataTables;
 
@@ -13,6 +13,6 @@ class Listing extends Controller
 {
     public function getAllAsResourceCollection()
     {
-        return DataTables::of(Collection::make(new CategoryCollection(new CategoryResource(Category::all()))))->make();
+        return DataTables::of(Collection::make(new CategoryCollection(new CategoryResource(MainCategory::all()))))->make();
     }
 }
