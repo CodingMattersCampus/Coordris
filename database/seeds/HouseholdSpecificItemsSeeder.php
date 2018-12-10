ab4ffb1b7791713826499b1fd824e3b8ac85cdab
robejamesdesierto <robejamesdesierto@gmail.com>
2018-12-10 18:02:26 +0800

    updating basic and specific items seeder

--- database/seeds/HouseholdSpecificItemsSeeder.php
+++ database/seeds/HouseholdSpecificItemsSeeder.php
@@ -50,5 +50,25 @@ class HouseholdSpecificItemsSeeder extends Seeder
             'quantity'  => 5,
             'age_range_id' => 0-3,
         ]);
+
+        \App\Models\Product\SpecificNeededItem::firstOrCreate([
+            'hh_support_code' => "2dfeb085-06ae-426f-8abb-2a87d33de446",
+            'top_level_category_id' => \App\Models\Product\Category\TopLevelCategory::getIdByName('Toddler'),
+            'main_category_id' => \App\Models\Product\Category\MainCategory::getIdByName("Food"),
+            'subcategory_id' => \App\Models\Product\Category\Subcategory::getIdByName("Soap"),
+            'unit'  => 'pcs',
+            'quantity'  => 5,
+            'age_range_id' => 4-6,
+        ]);
+
+        \App\Models\Product\SpecificNeededItem::firstOrCreate([
+            'hh_support_code' => "2dfeb085-06ae-426f-8abb-2a87d33de446",
+            'top_level_category_id' => \App\Models\Product\Category\TopLevelCategory::getIdByName('Toddler'),
+            'main_category_id' => \App\Models\Product\Category\MainCategory::getIdByName("Personal Hygiene"),
+            'subcategory_id' => \App\Models\Product\Category\Subcategory::getIdByName("Soap"),
+            'unit'  => 'pcs',
+            'quantity'  => 5,
+            'age_range_id' => 4-6,
+        ]);
     }
 }
