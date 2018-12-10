0d10a9bd237e9b4b83e798ff4326d802b70e0792
gabbydgab <gamba@gabbydgab.com>
2018-12-10 20:58:45 +0800

    add method to get (main) category name

--- app/Models/Product/ProductCategory.php
+++ app/Models/Product/ProductCategory.php
@@ -2,9 +2,15 @@
 
 namespace App\Models\Product;
 
+use App\Models\Product\Category\MainCategory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductCategory extends Model
 {
     protected $guarded = [];
+
+    public function categoryName()
+    {
+        return (MainCategory::find($this->category_id))->name;
+    }
 }
