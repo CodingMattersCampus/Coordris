3a9906736d93f0ccacbbfa8bd84ee4f6fdb9c3d8
robejamesdesierto <robejamesdesierto@gmail.com>
2018-12-10 10:23:51 +0800

    adding dummy data to the household specific items seeder

--- database/seeds/HouseholdSpecificItemsSeeder.php
+++ database/seeds/HouseholdSpecificItemsSeeder.php
@@ -13,12 +13,42 @@ class HouseholdSpecificItemsSeeder extends Seeder
     {
         \App\Models\Product\SpecificNeededItem::firstOrCreate([
             'hh_support_code' => "2dfeb085-06ae-426f-8abb-2a87d33de446",
-            'top_level_category_id' => \App\Models\Product\Category\TopLevelCategory::getIdByName('Female'),
-            'main_category_id' => \App\Models\Product\Category\MainCategory::getIdByName("Rice"),
-            'subcategory_id' => \App\Models\Product\Category\Subcategory::getIdByName("Milled Rice"),
-            'unit'  => 'kilo',
+            'top_level_category_id' => \App\Models\Product\Category\TopLevelCategory::getIdByName('Toddler'),
+            'main_category_id' => \App\Models\Product\Category\MainCategory::getIdByName("Food"),
+            'subcategory_id' => \App\Models\Product\Category\Subcategory::getIdByName("Milk"),
+            'unit'  => 'pcs',
             'quantity'  => 5,
-            'age_range_id' => 4,
+            'age_range_id' => 0-3,
+        ]);
+
+        \App\Models\Product\SpecificNeededItem::firstOrCreate([
+            'hh_support_code' => "2dfeb085-06ae-426f-8abb-2a87d33de446",
+            'top_level_category_id' => \App\Models\Product\Category\TopLevelCategory::getIdByName('Toddler'),
+            'main_category_id' => \App\Models\Product\Category\MainCategory::getIdByName("Food"),
+            'subcategory_id' => \App\Models\Product\Category\Subcategory::getIdByName("Cereal Milk"),
+            'unit'  => 'pcs',
+            'quantity'  => 5,
+            'age_range_id' => 0-3,
+        ]);
+
+        \App\Models\Product\SpecificNeededItem::firstOrCreate([
+            'hh_support_code' => "2dfeb085-06ae-426f-8abb-2a87d33de446",
+            'top_level_category_id' => \App\Models\Product\Category\TopLevelCategory::getIdByName('Toddler'),
+            'main_category_id' => \App\Models\Product\Category\MainCategory::getIdByName("Personal Hygiene"),
+            'subcategory_id' => \App\Models\Product\Category\Subcategory::getIdByName("Diapers"),
+            'unit'  => 'pcs',
+            'quantity'  => 5,
+            'age_range_id' => 0-3,
+        ]);
+
+        \App\Models\Product\SpecificNeededItem::firstOrCreate([
+            'hh_support_code' => "2dfeb085-06ae-426f-8abb-2a87d33de446",
+            'top_level_category_id' => \App\Models\Product\Category\TopLevelCategory::getIdByName('Toddler'),
+            'main_category_id' => \App\Models\Product\Category\MainCategory::getIdByName("Personal Hygiene"),
+            'subcategory_id' => \App\Models\Product\Category\Subcategory::getIdByName("Soap"),
+            'unit'  => 'pcs',
+            'quantity'  => 5,
+            'age_range_id' => 0-3,
         ]);
     }
 }
