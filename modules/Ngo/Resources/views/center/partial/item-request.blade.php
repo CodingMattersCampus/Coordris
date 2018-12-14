67de3a4cacb82b033f5cad04ba5a74e95655d57f
gabbydgab <gamba@gabbydgab.com>
2018-12-14 17:14:05 +0800

    initial implementation for item request listing

--- modules/Ngo/Resources/views/center/partial/item-request.blade.php
+++ modules/Ngo/Resources/views/center/partial/item-request.blade.php
@@ -1,1 +1,31 @@
-<p>Item request here</p>
+<table id="requested-items-table" class="table table-responsive table-striped table-hover">
+    <thead class="bg-blue-gradient">
+    <tr>
+        <th>Item</th>
+        <th>Needed</th>
+        <th>Given/Dispatched</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>Rice</td>
+        <td>{{$center->households()->pluck('rice')->sum()}}</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Canned Goods</td>
+        <td>{{$center->households()->pluck('canned_goods')->sum()}}</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Noodles</td>
+        <td>{{$center->households()->pluck('noodles')->sum()}}</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Water</td>
+        <td>{{$center->households()->pluck('water')->sum()}}</td>
+        <td></td>
+    </tr>
+    </tbody>
+</table>
